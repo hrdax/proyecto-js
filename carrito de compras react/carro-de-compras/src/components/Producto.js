90cbd1b8f112d5b6e3ee27f43de2dd2e2db1b6cf
hrdax <juanpa_200018@hotmail.com>
2023-07-30 20:15:08 -0400

    componente producto

---
+++ carrito de compras react/carro-de-compras/src/components/Producto.js
@@ -0,0 +1,21 @@
+import { Component } from 'react'
+
+class Producto extends Component {
+
+    render () {
+        const { producto } = this.props
+        console.log(this.props)
+        return(
+            <div>
+                <img alt={producto.name} src={ producto.img }/>
+                <h3>{ producto.name }</h3>
+                <p>{ producto.price }</p>
+            </div>
+        )
+    }
+
+}
+
+
+
+export default Producto
