4776ba2d42123fd33883482b9034b72ece9bc686
hrdax <juanpa_200018@hotmail.com>
2023-07-30 20:23:57 -0400

    estilos a productos

--- carrito de compras react/carro-de-compras/src/components/Producto.js
+++ carrito de compras react/carro-de-compras/src/components/Producto.js
@@ -1,13 +1,27 @@
 import { Component } from 'react'
 
+const styles = {
+    producto: {
+        border: 'solid 1px #eee',
+        boxShadow: '0 5px 5px rgb(0,0,0,0.1)',
+        width: '30%',
+        padding: '10px 15px',
+        borderRadius: '5px',
+        margin: '10px 15px'
+    },
+    img: {
+        width: '100%'
+    }
+}
+
 class Producto extends Component {
 
     render () {
         const { producto } = this.props
         console.log(this.props)
         return(
-            <div>
-                <img alt={producto.name} src={ producto.img }/>
+            <div style={ styles.producto }>
+                <img style={ styles.img } alt={producto.name} src={ producto.img }/>
                 <h3>{ producto.name }</h3>
                 <p>{ producto.price }</p>
             </div>
