8d0b2daa202a4345f314d78acc58ffcbb840e768
hrdax <juanpa_200018@hotmail.com>
2023-07-30 20:15:46 -0400

    agregando componentes

--- carrito de compras react/carro-de-compras/src/App.js
+++ carrito de compras react/carro-de-compras/src/App.js
@@ -1,12 +1,21 @@
 import { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Productos from './components/Productos'
 
 class App extends Component {
+  state ={
+    productos : [
+      {name: 'Tomate', price: 1500, img: '/productos/tomate.png'},
+      {name: 'Arbejas', price: 2500, img: '/productos/arbejas.png'},
+      {name: 'Lechuga', price: 500, img: '/productos/lechuga.png'},
+    ]
+  }
   render() {
     return (
       <div>
-        <h1>Primer comit</h1>
+        <Productos 
+          agregarAlCarro={() => console.log('mmmm')}
+          productos={this.state.productos}
+        />
       </div>
     )
   }
