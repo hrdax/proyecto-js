6902667d4c56e230b8a14c8c390bb88a11de62a2
hrdax <juanpa_200018@hotmail.com>
2023-07-31 09:43:53 -0400

    condicional de cantidad carro

--- carrito de compras react/carro-de-compras/src/App.js
+++ carrito de compras react/carro-de-compras/src/App.js
@@ -11,12 +11,18 @@ class App extends Component {
       {name: 'Arbejas', price: 2500, img: '/productos/arbejas.png'},
       {name: 'Lechuga', price: 500, img: '/productos/lechuga.png'},
     ],
-    carro : [
-      // {name: 'Tomate', price: 1500, img: '/productos/tomate.png', cantidad: 1},
-    ],
+    carro : [],
   }
   
 agregarAlCarro = (producto) => {
+  const { carro } = this.state
+  if (carro.find(x => x.name === producto.name)) {
+    const newCarro = carro.map(x => x.name === producto.name ? ({
+      ...x,
+      cantidad: x.cantidad + 1
+    }) : x)
+    return this.setState({ carro: newCarro })
+  }
   return this.setState({
     carro: this.state.carro.concat({
       ...producto,
