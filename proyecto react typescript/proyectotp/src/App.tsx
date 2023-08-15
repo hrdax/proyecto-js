727f74175af11126f8daad2b518726220e7ab6af
hrdax <juanpa_200018@hotmail.com>
2023-08-15 14:25:53 -0400

    primer comit entorno y bulma css

---
+++ proyecto react typescript/proyectotp/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className='button is-primary'>Probando bulma</button>
+    </div>
+  );
+}
+
+export default App;
