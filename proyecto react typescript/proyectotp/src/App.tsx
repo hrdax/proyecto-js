ead3ea302ce90d202ebc6e44161c0efe5dfd231c
hrdax <juanpa_200018@hotmail.com>
2023-08-15 14:43:03 -0400

    componente input y handlechange

--- proyecto react typescript/proyectotp/src/App.tsx
+++ proyecto react typescript/proyectotp/src/App.tsx
@@ -1,15 +1,23 @@
-import React, { MouseEventHandler } from 'react';
+import React, { ChangeEventHandler, MouseEventHandler } from 'react';
 import 'bulma/css/bulma.css';
 import Button from './components/Button';
+import Input from './components/Input';
 import './App.css';
 
 function App() {
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+  }
+
   const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault();
     console.log('click');
   }
   return (
     <div className="App">
+      <Input placeholder='Nombre' handleChange={ handleChange } />
       <Button handleCLick={ handleClick }>Probando componente</Button>
     </div>
   );
